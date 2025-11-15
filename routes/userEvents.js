f679cc98576a4eba758b89e64b26240713a67e15
Loris Catizzone <loriscatiz@gmail.com>
2025-11-15 15:33:16 +0100

    adds put getById and delete

--- routes/userEvents.js
+++ routes/userEvents.js
@@ -7,6 +7,34 @@ router.get('/', async (req, res, next) => {
     res.send(events)
 })
 
+router.get('/:id', async (req, res, next) => {
+    id = req.params.id
+    const eventObj = await UserEvent.findOne({ _id: id })
+    if (eventObj) {
+        res.status(200)
+        return res.send(eventObj)
+    }
+    res.status(404)
+    return res.send()
+})
+
+router.put('/:id', async (req, res, next) => {
+    id = req.params.id
+    const updatedEventData = req.body
+
+    const resEvent = await UserEvent.updateOne({ _id: id }, updatedEventData)
+
+    return res.send([resEvent, await UserEvent.findById(id)])
+})
+
+router.delete('/:id', async (req, res, next) => {
+    id = req.params.id
+
+    const resEvent = await UserEvent.deleteOne({ _id: id })
+
+    return res.send(resEvent)
+})
+
 router.post('/', async (req, res, next) => {
     const newEventData = req.body
     const newEvent = new UserEvent(newEventData)
